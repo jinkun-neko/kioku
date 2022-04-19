342f3e22107e9b7e4ce1f54e8976b5d694b1092b
jinkun <jin2jin700@gmail.com>
2022-04-19 12:21:54 +0900

    12:21 firstcommit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+    def title(page_title = '')
+      kioku_title = "KIOKU"
+          if page_title.empty?
+              shiori_title
+          else
+              page_title + "○" + kioku_title
+          end
+      end
+  end

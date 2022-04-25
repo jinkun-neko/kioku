86044214066397477914750a017e64cdff1ea87e
jinkun <jin2jin700@gmail.com>
2022-04-26 00:03:42 +0900

    jinkun

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '90882d498670178908c8bd0ea203c633f0929ef5a944742cb9306fcbdeb567d6d7a39781542dfad1d7cfe0bd15033c91d2bc734c1a677cb4462dc4888bfbe24c'

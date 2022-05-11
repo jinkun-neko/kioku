aba34fc2101c35725930ae90955f600b20da01d3
jinkun <jin2jin700@gmail.com>
2022-05-11 12:14:27 +0900

    seeds dete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# user情報サンプル
+User.create!(name:  "jinkun",
+  email: "jin2jin700@gmail.com",
+  password:  "jinjinjinjin",
+  password_confirmation: "jinjinjinjin",
+)
+User.create!(name: "サボぼぼ",
+  email: "jin2jin700@gmail.com",
+  password:  "jinjin",
+  password_confirmation: "jinjin",
+)
+User.create!(name:  "ゆうくん",
+  email: "yuukun@gmail.com",
+  password: "yuuyuu",
+  password_confirmation: "yuuyuu",
+)
+User.create!(name:  "shiori",
+  email: "shiori727@gmail.com",
+  password:  "shioriLM",
+  password_confirmation: "shioriLM",
+)
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  body  = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(body: content) }
+end

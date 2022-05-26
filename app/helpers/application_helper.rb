7199a5cf4eb0fcdee9a0a90dce0646755062563a
jinkun <jin2jin700@gmail.com>
2022-05-26 11:16:06 +0900

    README edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-    def title(page_title = '')
+
+    def title(page_title ='')
         kioku_title = "KIOKU"
-            if page_title.empty?
-                shiori_title
-            else
-                page_title + "○" + kioku_title
-            end
+        if page_title.empty?
+            kioku_title
+        else
+            page_title + "○" + kioku_title
         end
     end
+end

863e7f406c670678e96fd6bc9e0cc1d3e24e19e2
jinkun <jin2jin700@gmail.com>
2022-06-01 12:01:17 +0900

    rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username                 {"jinjin"}
+    email                 {"test@gmail.com"}
+    password              {"111111"}
+    password_confirmation {"111111"}
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,30 +3,29 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get  posts_path
+      get posts_path , params: {id: post.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get post_path
+      get post_path , params: {id: post.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get new_post_path
+      get new_post_path , params: {id: post.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get edit_post_path
+      get edit_post_path , params: {id: post.id}
       expect(response).to have_http_status(:success)
     end
   end
-
 end
